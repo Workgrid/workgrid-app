8f4000bd20c47860761c5fcc8495e65a2867825f
Corey Vaillancourt <corey.vaillancourt@workgrid.com>
2021-02-02 07:56:11 -0500

    ETSWORK-9564 - Add and configure react-i18next to support internationalization.
    `react-18next` was the library chosen by Workgrid Engineering to use in our client applications. This initial configuration assumes we'll be able to fetch translations over the network at some point in the future or if we deploy as as webapp. In its present form for mobile the translations will be bundled with the application. Even though we're still using the blank Ionic template I've provided an example usage of the translation hook to demonstrate how it works. This is only temporary and it will be removed once we begin actual development of features.

---
+++ src/i18n.ts
@@ -0,0 +1,33 @@
+import i18n from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import I18NextChainedBackend from 'i18next-chained-backend'
+import I18NextHttpBackend from 'i18next-http-backend'
+import I18NextLocalStorageBackend from 'i18next-localstorage-backend'
+import { initReactI18next } from 'react-i18next'
+
+i18n
+  .use(LanguageDetector)
+  .use(I18NextChainedBackend)
+  .use(initReactI18next)
+  .init({
+    ns: ['common', 'home'],
+    lng: 'en-US',
+    fallbackLng: 'en-US',
+    interpolation: {
+      escapeValue: false // not needed for react as it escapes by default
+    },
+    backend: {
+      backends: [
+        I18NextLocalStorageBackend, // primary
+        I18NextHttpBackend // This will allow us to centrally host translations that can be pulled by any app
+      ],
+      backendOptions: [
+        {},
+        {
+          loadPath: 'locales/{{lng}}/{{ns}}.json' // We change the default path here to remove the leading slash so Electron can work
+        }
+      ]
+    }
+  })
+
+export default i18n
